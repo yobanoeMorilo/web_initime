8aa6feaa8359a349edcc41eae41abcd0b825e6c1
yobanoeMorilo <temmets20@gmail.com>
2023-02-20 14:26:50 +0700

    Page transition and regexps for email, password

---
+++ js/landing.js
@@ -0,0 +1,12 @@
+
+$("#log_in").bind({
+    click: function() {
+        window.location.href = "login.html";
+    }
+  });
+
+$("#sign_in").bind({
+    click: function() {
+        window.location.href = "registration.html";
+    }
+  });
