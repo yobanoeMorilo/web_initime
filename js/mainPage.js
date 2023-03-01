5f4c41bba02dd373400623165561ca92a2305c30
yobanoeMorilo <temmets20@gmail.com>
2023-03-01 23:13:37 +0700

    Modals functionality

--- js/mainPage.js
+++ js/mainPage.js
@@ -5,20 +5,72 @@ const inputProffessor = $('#inputProffessor')
 const inputAuditory = $('#inputAuditory')
 const addModal = $("#addModal")
 const delModal = $("#delModal")
+const editModal = $("#editModal")
+const examplePair = $("#template")
+
+let editing_cell
+
+//Логика модальных окон
+
+$("#edit_btn_modal").on('click', function(){
+
+  //запрос
+  let proff = $("#edit_discipline").val()
+  let audit = $("#edit_auditory").val()
+  let discip = $("#edit_discipline").val()
+  let type = $("#edit_type").val()
+
+  $(editing_cell).find('#Lesson').text(discip);
+  $(editing_cell).find('#Auditory').text(audit);
+  $(editing_cell).removeClass()
+  $(editing_cell).addClass("timeslot")
+  choseType($(editing_cell), type)
+
+  editModal.modal('hide')
+
+  editing_cell = null
+})
+
+$("#addPairBtn").on('click', function(){
+
+  addPairModal(editing_cell)
+  addModal.modal('hide')
+
+})
+
+$("#deletePair").on('click', function(){
+  //send delete request and recieve an good awnser
+  deletePairModal(editing_cell)
+  delModal.modal('hide')
+
+})
+
+//Функционал открытия / закрытия модальных окон
+
+$("#close_BTN").on('click', function(){
+  addModal.modal('hide')
+})
 
 $("#disableModal").on('click', function(){
   delModal.modal('hide')
+  editModal.modal('show')
 })
 
+$("#delete_btn").on('click', function(){
+  editModal.modal('hide')
+  delModal.modal('show')
+})
 
-  $("#tableHolder tbody tr td ").on('click', function(event){
-    console.log(event.target.localName == "td")
+$("#tableHolder tbody tr td ").on('click', function(event){
+
     if(event.target.localName == "td") {
+      editing_cell = event.currentTarget
       addModal.modal('show')
     }
 
     if(event.target.localName == "div") {
-      delModal.modal('show')
+      editing_cell = event.currentTarget.lastElementChild
+      editModal.modal('show')
     }
   })
 
@@ -59,6 +111,39 @@ inputFaculty.change(function() {
     });
 })
 
+function addPairModal(elem){
+  let pair = examplePair.clone()
+  //запрос
+  let proff = $("#add_discipline").val()
+  let audit = $("#add_auditory").val()
+  let discip = $("#add_discipline").val()
+  let type = $("#add_type").val()
+
+  choseType(pair, type)
+  pair.id = ""
+  pair.find("#Lesson").text(discip)
+  pair.find("#Auditory").text(audit)
+
+  elem.append(pair[0])
+}
+
+function deletePairModal(elem){
+  elem.remove()
+}
+
+function choseType(elem, type){
+  switch (type){
+    case 'Лекция': elem.addClass("lection")
+      break;
+    case 'Семинар': elem.addClass("seminar")
+      break;
+    case 'Практика': elem.addClass("practice")
+      break;
+    case 'Контрольная точка': elem.addClass("control")
+      break;
+  }
+}
+
 function response(url, elem){
   sendRequest(url)
     .then(response => {
@@ -76,11 +161,15 @@ function navTeacher(){
   inputProffessor.removeClass("d-none")
   let url = 'https://localhost:7272/api/teachers'
 
+  sessionStorage.setItem('mainPageSession', 'teacher')
+
   response(url, inputProffessor)
 }
 
 function navAuditory(){
   inputAuditory.removeClass("d-none")
+
+  sessionStorage.setItem('mainPageSession', 'auditory')
 
   let url = 'https://localhost:7272/api/auditories'
   response(url, inputAuditory)
@@ -90,6 +179,8 @@ function navGroup(){
   inputDirection.removeClass("d-none")
   inputFaculty.removeClass("d-none")
   inputGroup.removeClass("d-none")
+
+  sessionStorage.setItem('mainPageSession', 'group')
 
   let url = 'https://localhost:7272/api/faculties'
   response(url, inputFaculty)
@@ -111,17 +202,27 @@ function attachDirections(response, elem){
   });
 }
 
+function checkCookies(target){
+  switch(target){
+    case "auditory" : navAuditory()
+    return true
+    case "teacher" : navTeacher()
+    return true
+    case "group" : navGroup()
+    return true
+    default: return false
+  }
+}
+
 
 function main(){
   var target = (document.cookie).split('=')[1]
-  switch(target){
-    case "auditory" : navAuditory()
-    break;
-    case "teacher" : navTeacher()
-    break;
-    case "group" : navGroup()
-    break;
-  }
+
+  if(!checkCookies(target))
+    target = sessionStorage.getItem('mainPageSession')
+
+  if(!checkCookies(target))
+    window.location.href = "path.html";
 }
 
 main()
